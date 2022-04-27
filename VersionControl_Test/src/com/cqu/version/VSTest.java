cd48ea17e0201fedce7d9e02b92367a1258a6dc6
cjx <jxchan@163.com>
2022-04-27 13:43:17 +0800

    third commit by branch master!

--- VersionControl_Test/src/com/cqu/version/VSTest.java
+++ VersionControl_Test/src/com/cqu/version/VSTest.java
@@ -4,5 +4,6 @@ public class VSTest {
     public static void main(String[] args) {
         System.out.println("VSTest versionOne, on branch master!!!");
         System.out.println("VSTest versionTwo, on branch master!!!");
+        System.out.println("VSTest versionThird, on branch master!!!");
     }
 }
