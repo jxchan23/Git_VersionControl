4fe3396adf4574a5476e4ba00351cbfa29cf5116
cjx <jxchan@163.com>
2022-04-27 13:42:49 +0800

    second commit by branch master!

--- VersionControl_Test/src/com/cqu/version/VSTest.java
+++ VersionControl_Test/src/com/cqu/version/VSTest.java
@@ -3,5 +3,6 @@ package com.cqu.version;
 public class VSTest {
     public static void main(String[] args) {
         System.out.println("VSTest versionOne, on branch master!!!");
+        System.out.println("VSTest versionTwo, on branch master!!!");
     }
 }
